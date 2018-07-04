c1ab644029d3ae64124db6516421e886cbb88c0c
akari0624 <akari0624@hotmail.com>
2018-07-04 15:25:13 +0800

    config file-loader to pack image file

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,60 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill' ,'./src/index.js'],
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/
-            }, {
-                use: [
-                    {
-                        loader: 'style-loader'
-                    }, {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'less-loader',
-                        options: {
-                            javascriptEnabled: true
-                        }
-                    }
-                ],
-                test: /\.(css|less)$/
-
-            }, {
-                use: 'file-loader',
-                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
-
-            }, {
-                use: 'url-loader?limit=100000',
-                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
+  entry: ['babel-polyfill' ,'./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },  
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
             }
-
-        ]
-    },
-    plugins: [new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-            filename: './index.html'
-        })],
-    resolve: {
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            'node_modules'
+          }
         ],
-        extensions: ['*', '.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        port: 9999
-    }
-};
+        test: /\.(css|less)$/  
+      }, {
+        test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/,
+        use:[
+          {
+            loader:'file-loader',
+            options:{
+              name:'[path][name].[ext]',
+              publicPath:''
+            }
+          }
+        ]   
+      }  
+    ]
+  },
+  plugins: [new HtmlWebPackPlugin({
+    template: path.resolve(__dirname, 'index.html'),
+    filename: './index.html'
+  })],
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 9999
+  }
+}  
